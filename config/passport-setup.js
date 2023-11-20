240dceab4bace03e92884b696fb96ad366e25d6b
Sanni-Damilola <sannifortune11@gmail.com>
2023-11-20 15:25:43 +0100

    added

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,15 @@
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
 
+console.log("c", keys.google.clientID);
+console.log("s", keys.google.clientSecret);
 passport.use(
   new GoogleStrategy({
     // options for google strategy
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
   }),
   () => {
     // pasport callback function
