2f0d2e0eef9b25b20507769ecca051d77ca4474f
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-04 04:34:17 +0100

    added

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -37,6 +37,7 @@ passport.use(
 
       // Save the new user to the database
       const savedUser = await newUser.save();
+      done(Error | null, newUser);
 
       // Pass the user to the done callback
       done(null, savedUser);
