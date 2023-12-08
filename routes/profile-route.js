f3c40dc39aa8747900af5b3feb5d59e690ccc526
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-08 13:45:18 +0100

    added

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,5 +1,14 @@
 const profileRouter = require("express").Router();
 
+const authCheck = (req, res, next) => {
+  if (!req?.user) {
+    //if user is not logged in
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
+};
+
 profileRouter.get("/", (req, res) => {
   res.send(
     "Successfully Logged In, This is Your Profile Page - " + req.user?.username
