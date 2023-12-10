57bdd610d9f1dae1f689c77f619ed554b2158c83
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-10 14:09:12 +0100

    added

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 
 // auth login
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { user: req.user });
 });
 
 // auth loggout
